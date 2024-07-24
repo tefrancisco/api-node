768c3c7c9f35b76539080ec4b598db21c18a54c1
tefrancisco <dzfranciscodz@gmail.com>
2024-07-23 21:00:23 -0300

    Node API with Postgres

---
+++ server.js
@@ -0,0 +1,57 @@
+// Craindo o servidor com fastify
+
+import { fastify } from 'fastify'
+// import { databaseMemory } from './database-memory.js'
+import { databasePostgres } from './database-postgres.js'
+
+const server = fastify()
+
+const database = new databasePostgres()
+
+// Request body
+
+server.post('/videos' , async (request, reply) => {
+    const {title, description, duration} = request.body
+
+    await database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(201).send()
+})
+
+server.get('/videos', async (request) => {
+    const search = request.query.search
+
+    const videos = await database.list(search)
+
+    return videos
+})
+
+server.put('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id
+    const {title, description, duration} = request.body
+
+    await database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204)
+})
+
+server.delete('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id
+
+    await database.delete(videoId)
+
+    return reply.status(204).send()
+})
+
+server.listen({
+    port: process.env.PORT ?? 3333,
+})
+
